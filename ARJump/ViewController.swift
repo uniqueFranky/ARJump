43d100a840335ca9fcd55ff4c9d9a3c3bf29e493
uniqueFranky <y_r_b@qq.com>
2022-07-15 15:37:04 +0800

    Add Plane Detection

--- ARJump/ViewController.swift
+++ ARJump/ViewController.swift
@@ -9,66 +9,87 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
-        sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        configureSceneView()
+        configureScene()
     }
     
+    //run session
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        //detect horizontal planes
+        configuration.planeDetection = [.horizontal]
         sceneView.session.run(configuration)
     }
     
+    //pause session
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
+}
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+
+//Configure Functions
+extension ViewController {
+    func configureSceneView() {
+        sceneView.delegate = self
+        sceneView.debugOptions = [.showWorldOrigin, .showPhysicsShapes]
+        sceneView.showsStatistics = true
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    func configureScene() {
+        let scene = SCNScene()
+        sceneView.scene = scene
     }
 }
+
+//ARSCNView Delegate
+extension ViewController: ARSCNViewDelegate {
+    //Detected a Plane
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        //Only process anchors for planes
+        guard let planeAchor = anchor as? ARPlaneAnchor else {
+            return
+        }
+        guard let meshGeometry = ARSCNPlaneGeometry(device: sceneView.device!) else {
+            fatalError()
+        }
+        meshGeometry.update(from: planeAchor.geometry)
+        let meshNode = SCNNode(geometry: meshGeometry)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.blue
+        meshNode.geometry?.materials = [material]
+        meshNode.opacity = 0.5
+        
+        node.addChildNode(meshNode)
+    }
+    
+    //Update Plane
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {
+            return
+        }
+        guard let meshNode = node.childNodes.first else {
+            return
+        }
+        guard let meshGeometry = meshNode.geometry as? ARSCNPlaneGeometry else {
+            return
+        }
+
+        meshGeometry.update(from: planeAnchor.geometry)
+    }
+}
+
+
+
